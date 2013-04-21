9cba69255b6d72f377760037f3fc04121e9c3186
Scott Cagno <scottiecagno@gmail.com>
2013-04-20 23:34:03 -0400

    first commit

---
+++ conf_kit.go
@@ -0,0 +1,83 @@
+// -----------
+// conf_kit.go ::: config file kit & helpers
+// -----------
+// Copyright (c) 2013-Present, Scott Cagno. All rights reserved.
+// This source code is governed by a BSD-style license.
+
+package conf_kit
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+	"time"
+)
+
+// config structure
+type Config struct {
+	fpath string
+	data  map[string]interface{}
+	mu    sync.Mutex
+}
+
+// create new config structure instance
+// return pointer to instance
+func NewConfig(fpath string) *Config {
+	return &Config{fpath: fpath}
+}
+
+// watch config file, update data map every time file changes
+func (self *Config) Watch(n int64, debug bool) {
+	// check to see if file has changed since n seconds ago
+	if changed(self.fpath, n) {
+		// file changed; lock mutex to make data map threadsafe
+		self.mu.Lock()
+		// update data map with new data from file
+		self.data = update(self.fpath)
+		// unlock mutex and proceed
+		self.mu.Unlock()
+		// check debug status, if true print info
+		if debug {
+			log.Printf("%q changed. updated map data\n", self.fpath)
+		}
+	}
+	// wait for n seconds then recurse; aka: call this func again from within itself
+	time.AfterFunc(time.Duration(n)*time.Second, func() { self.Watch(n, debug) })
+}
+
+// return updated map
+func update(fpath string) map[string]interface{} {
+	// read file data (json []byte) into b
+	b, err := ioutil.ReadFile(fpath)
+	// err check
+	if err != nil {
+		// if err; print timestamp and err, then panic
+		log.Panic(err)
+	}
+	// no err; create empty map placeholder
+	var m map[string]interface{}
+	// unmarshal b (json []byte) into map
+	err = json.Unmarshal(b, &m)
+	// err check
+	if err != nil {
+		// if err; print timestamp and err, then panic
+		log.Panic(err)
+	}
+	// no err; return map 
+	return m
+}
+
+// return true if file has been modified within n seconds, else return false
+func changed(fpath string, n int64) bool {
+	// gather file status
+	fstat, err := os.Stat(fpath)
+	// err check
+	if err != nil {
+		// if err; print timestamp and err, then panic
+		log.Panic(err)
+	}
+	// no err; eval and return wether file been modified within the last n seconds
+	return fstat.ModTime().Unix() >= time.Now().Unix()-n
+}
---
+++ example/example.go
@@ -0,0 +1,30 @@
+// ----------
+// example.go ::: example usage for conf_kit
+// ----------
+// Copyright (c) 2013-Present, Scott Cagno. All rights reserved.
+// This source code is governed by a BSD-style license.
+
+package main
+
+import (
+	"conf_kit"
+	"fmt"
+)
+
+/*
+ *  1) compile and execute this file
+ *	2) open config/mycfg.json
+ * 	3) simply save or change data and save
+ * 	4) update notification (debug is true) should print within n (3) seconds
+ */
+
+func main() {
+
+	// setup new config location
+	cfg := conf_kit.NewConfig("config/mycfg.json")
+	// run watch method; check the file every 10 seconds, debug mode set to true
+	cfg.Watch(3, true)
+
+	// used to halt program; otherwise program will exit right after the watch function runs
+	func() { fmt.Println("save/change config file, any key will exit..."); var n int; fmt.Scanln(&n) }()
+}
